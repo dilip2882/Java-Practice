04f3911c02a973c27e66738a019f1ed074ade5f1
Dilip Patel <pateldilip2902@gmail.com>
2024-02-18 20:47:16 +0530

    Java Programs

---
+++ 4 Opertors, If else & Number System/Challenges/src/OddEvenBitwise.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class OddEvenBitwise {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Bitwise Odd Even Number Checker");
+        System.out.print("Enter your number: ");
+        int num = input.nextInt();
+        if ((num & 1) == 1) {
+            System.out.println("Your number is odd");
+        } else {
+            System.out.println("Your number is even");
+        }
+
+    }
+}
