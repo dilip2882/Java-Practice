5072fb055b06c2e21aea122d4a15a8969e9c7c72
Dilip Patel <pateldilip2902@gmail.com>
2024-02-28 18:01:57 +0530

    Add Java Programs

--- 5 While Loop, Methids and Arrays/Challenges/src/Pattern.java
+++ 5 While Loop, Methids and Arrays/Challenges/src/Pattern.java
@@ -5,9 +5,16 @@ public class Pattern {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter rows: ");
         int maxRows = input.nextInt();
-        pattern4(maxRows);
+        pattern6(maxRows);
 
     }
+
+//    pattern1:
+//            * * * * *
+//            * * * * *
+//            * * * * *
+//            * * * * *
+//            * * * * *
 
     static void pattern1(int maxRows) {
         for (int row = 1; row <= maxRows ; row++) {
@@ -20,10 +27,16 @@ public class Pattern {
         }
     }
 
+//pattern2:
+//            *
+//            * *
+//            * * *
+//            * * * *
+//            * * * * *
     static void pattern2(int maxRows) {
         for (int row = 1; row <= maxRows ; row++) {
             //for every row, run the col
-            for (int col = 1; col <= row; col--) {
+            for (int col = 1; col <= row; col++) {
                 System.out.print("* ");
             }
             // when one row is printed, we need to add a newline
@@ -31,6 +44,12 @@ public class Pattern {
         }
     }
 
+//    pattern3:
+//            * * * * *
+//            * * * *
+//            * * *
+//            * *
+//            *
     static void pattern3(int maxRows) {
         for (int row = 1; row <= maxRows ; row++) {
             //for every row, run the col
@@ -42,7 +61,7 @@ public class Pattern {
         }
     }
 
-//    pattern 4:
+//    pattern4:
 //            1
 //            12
 //            123
@@ -59,4 +78,84 @@ public class Pattern {
             System.out.println();
             }
         }
+
+//        pattern5:
+//            *
+//            * *
+//            * * *
+//            * * * *
+//            * * * * *
+//            * * * *
+//            * * *
+//            * *
+//            *
+    static void pattern5A(int maxRows) {
+        for (int row = 1; row <= maxRows ; row++) {
+            //for every row, run the col
+            for (int col = 1; col <= row; col++) {
+
+                System.out.print("* ");
+            }
+            // when one row is printed, we need to add a newline
+            System.out.println();
+        }
+
+        for (int row = 1; row <= maxRows ; row++) {
+            //for every row, run the col
+             for (int col = maxRows - 1; col >= row; col--) {
+
+                 System.out.print("* ");
+             }
+             // when one row is printed, we need to add a newline
+             System.out.println();
+        }
+    }
+
+        static void pattern5B(int maxRows) {
+            for (int row = 1; row < 2 * maxRows; row++) {
+
+                // Logic
+                //explanation:
+                //if (row > maxRows) {
+                //    maxRows - (row - maxRows) = 2 * maxRows - row
+                //}
+                int totalColsInRow = row > maxRows ? 2 * maxRows - row : row;
+
+                // Print the stars
+                for (int col = 1; col <= totalColsInRow; col++) {
+                    System.out.print("* ");
+                }
+
+                // Add a newline after each row
+                System.out.println();
+            }
+        }
+
+//        pattern6:
+    static void pattern6(int maxRows) {
+        for (int row = 1; row < 2 * maxRows; row++) {
+
+            // Logic
+            //explanation:
+            //if (row > maxRows) {
+            //    maxRows - (row - maxRows) = 2 * maxRows - row
+            //}
+            int totalColsInRow = row > maxRows ? 2 * maxRows - row : row;
+
+            //Logic for Spaces
+            int noOfSpaces = maxRows - totalColsInRow;
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+
+            // Print the stars
+            for (int col = 1; col <= totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+
+            // Add a newline after each row
+            System.out.println();
+        }
+    }
+
 }
