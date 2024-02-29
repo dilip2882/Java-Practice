ccb3fc5d7bdeb56ada2aa5bff1549e01ec903648
Dilip Patel <pateldilip2902@gmail.com>
2024-02-29 13:34:36 +0530

    Add Java Programs

---
+++ 5 While Loop, Methids and Arrays/Challenges/src/ArrayUtility.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArrayUtility {
+    public static int[] inputArray() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Array sum and Average");
+        System.out.print("Please the Enter the no of elements: ");
+        int size = input.nextInt();
+
+        int[] nums = new int[size];
+        int i = 0;
+        while (i < size) {
+            System.out.print("Please enter element no: " + (i+1) + ":");
+            nums[i] = input.nextInt();
+            i++;
+        }
+        return nums;
+    }
+}
