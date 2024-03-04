78230bb2bdf2e5fa56d359edd4b930c35a55e3b6
Dilip Patel <pateldilip2902@gmail.com>
2024-03-04 22:21:07 +0530

    Add Java Programs

---
+++ 5 While Loop, Methids and Arrays/Challenges/src/SumOfDiaognal2DArray.java
@@ -0,0 +1,54 @@
+public class SumOfDiaognal2DArray {
+    public static void main(String[] args) {
+        System.out.println("Sum of Diagonals of NxN 2D Array");
+        int[][] numArr = ArrayUtility.input2DArray();
+        long sum = sumOfLeftDiagonal(numArr) + sumOfRightDiagonal(numArr);
+        if (numArr.length % 2 != 0) {
+            int index = numArr.length / 2;
+            sum -= numArr[index][index];
+        }
+        System.out.println("Sum of Diagonals of 2D Array is " + sum);
+    }
+
+    public static long sumOfLeftDiagonal(int[][] numArr) {
+        long sum = 0;
+        int i = 0;
+        while (i < numArr.length) {
+            sum += numArr[i][i];
+            i++;
+        }
+        return sum;
+    }
+
+    public static long sumOfRightDiagonal(int[][] numArr) {
+        long sum = 0;
+        int i = 0;
+        while (i < numArr.length) {
+            int col = numArr.length - 1 - i;
+            sum += numArr[i][col];
+            i++;
+        }
+        return sum;
+    }
+
+//    public static long sum(int[][] numArr) {
+//        long sum = 0;
+//        int i = 0;
+//        while (i < numArr.length) {
+//            int j = 0;
+//            while (j < numArr.length) {
+//                // Check for main diagonal element.
+//                if (i == j) {
+//                    sum += numArr[i][j];
+//                }
+//                // Check for secondary diagonal element.
+//                if (i + j == numArr.length - 1) {
+//                    sum += numArr[i][j];
+//                }
+//                j++;
+//            }
+//            i++;
+//        }
+//        return sum;
+//    }
+}
